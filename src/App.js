8e9ccb00ceb67c1771b44b95118b9f79be4f45cb
Lucas Rueda <lucasrueda64@gmail.com>
2022-08-25 21:17:23 -0300

    ArgenSteam

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aca voy a hacer mi Steam Argentino pá.
         </a>
       </header>
     </div>
